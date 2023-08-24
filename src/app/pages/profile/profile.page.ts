ad92a1c1b875b9608339924be4a0dbfa4f15ee7b
danielcastillopalma <d.castillo3@duocuc.cl>
2023-08-24 18:44:16 -0400

    m

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, VERSION, Inject, ViewChild, Renderer2, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
-
+  constructor( private element: ElementRef, private router: Router, private activateRoute: ActivatedRoute) {
+    this.activateRoute.queryParams.subscribe(params=>{
+      if(this.router.getCurrentNavigation()?.extras.state){
+        this.data=this.router.getCurrentNavigation()?.extras.state?.["user"];
+        console.log(this.data)
+      }else{
+        this.data=["Mi Promedio"]
+      }
+    });
+  }
+  primario="primarioBasico";
+  data:any;
+  secundario="secundarioBasico";
+  terciario="terciarioBasico";
+  cuarto="cuartoBasico";
   ngOnInit() {
   }
-
+  logIn(){
+    this.router.navigate(['/login']);
+    this.data="";
+    console.log(this.data)
+  }
 }
