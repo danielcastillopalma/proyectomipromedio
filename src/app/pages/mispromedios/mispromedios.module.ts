f5ccf67ce71bb3b8c674abc15a37a394812f8812
danielcastillopalma <d.castillo3@duocuc.cl>
2023-11-09 17:40:40 -0300

    cambios visuales

--- src/app/pages/mispromedios/mispromedios.module.ts
+++ src/app/pages/mispromedios/mispromedios.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { MispromediosPageRoutingModule } from './mispromedios-routing.module';
 
 import { MispromediosPage } from './mispromedios.page';
+import { MenuModule } from 'src/app/modules/menu/menu.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MispromediosPageRoutingModule
+    MispromediosPageRoutingModule,
+    MenuModule
   ],
   declarations: [MispromediosPage]
 })
--- src/app/pages/mispromedios/mispromedios.page.ts
+++ src/app/pages/mispromedios/mispromedios.page.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { coloresBasicos, coloresDuoc } from '../../app.module'
+import { LoadingController, ToastController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
+import { LocalNotifications, LocalNotificationsPlugin, ScheduleOptions } from '@capacitor/local-notifications'
+import { Calendar } from '@awesome-cordova-plugins/calendar/ngx';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-mispromedios',
@@ -6,10 +13,222 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mispromedios.page.scss'],
 })
 export class MispromediosPage implements OnInit {
+  nombrePromArit: any = "";
+  token = "";
+  tipoPromedio: any[] = [
+    { prom: 1, tipo: "Aritmético" },
+    { prom: 2, tipo: "Ponderado" },
+    { prom: 3, tipo: "Otro" },
+  ]
+  data: any;
+  tipos: any = {
+    tipo: ""
+  }
+  promArit: any[] = [
+    { pos: 1, notArit: '' },
+    { pos: 2, notArit: '' },
+    { pos: 3, notArit: '' },
 
-  constructor() { }
+  ]
+  promPonde: any[] = [
+    { pos: 1, notPond: '', porcPond: '' },
+    { pos: 2, notPond: '', porcPond: '' },
+    { pos: 3, notPond: '', porcPond: '' },
+  ]
 
-  ngOnInit() {
+  cuarto = coloresBasicos.cuarto;
+  terciario = coloresBasicos.terciario;
+  secundario = coloresBasicos.secundario;
+  primario = coloresBasicos.primario;
+  userData: any = ""
+  userDataEmail: any = ""
+
+  constructor(
+    private storage: Storage,
+    private router: Router,
+    private loadingCtrl: LoadingController,
+    private calendar: Calendar,
+    private db: DatabaseService,
+    private toastCtrl: ToastController,
+  ) {
+
+    this.userData = JSON.parse(localStorage.getItem('usuario')!);
+    this.userDataEmail = JSON.parse(localStorage.getItem('email')!);
+
+
+
+  }
+  @ViewChild('promedioBasico') promedioBasico: ElementRef;
+  @ViewChild('promedioPorcentual') promedioPorcentual: ElementRef;
+  async ngOnInit() {
+    await this.storage.create();
+    LocalNotifications.checkPermissions();
+    LocalNotifications.requestPermissions();
+    console.log("userdata")
+    console.log(this.userData);
+    console.log("userdata")
+
   }
 
+  sumarPromArit = 0;
+  totalPromArit = 0;
+  promedioAritmetico = 0;
+  cantDelArit = 0;
+  //reloadbutton
+  refresh() {
+    window.location.reload();
+  }
+
+  //notificaciones
+  async scheduleNotification() {
+    this.calendar.createEventInteractively(
+      'Titulo',
+      'Ubicacion',
+      undefined,
+      new Date(),
+      undefined
+    )
+
+    let options: ScheduleOptions = {
+      notifications: [
+        {
+          id: 1,
+          title: "titulo notificacion",
+          body: "Cuerpo de la notificación",
+          largeBody: "Cuerpo grande de la notificacion",
+          summaryText: "Texto bait"
+        }
+      ]
+    }
+    try {
+      await LocalNotifications.schedule(options)
+    } catch (ex) {
+      alert(JSON.stringify(ex));
+    }
+
+  }
+
+  loginForSave() {
+    this.presentToast("Inicia sesión para guardar tus promedios")
+
+  }
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 2000, // la duracion toast 
+      position: 'bottom', // en donde va el toast
+      color: 'warning', // 
+    });
+    toast.present();
+  }
+
+  calcularPromArit() {
+    let cant = Object.keys(this.promArit).length;
+    for (let nota of this.promArit) {
+      if (nota.notArit == '') {
+        this.sumarPromArit = this.sumarPromArit + 0
+        this.totalPromArit = this.sumarPromArit / cant
+      } else {
+        this.sumarPromArit = this.sumarPromArit + parseInt(nota.notArit)
+
+        this.totalPromArit = this.sumarPromArit / cant
+      }
+    }
+    this.promedioAritmetico = this.totalPromArit
+    console.log(this.totalPromArit)
+    console.log(this.sumarPromArit)
+
+    this.sumarPromArit = 0;
+
+  }
+  agregarNotaArit() {
+    if (this.cantDelArit > 0) {
+      let cant = Object.keys(this.promArit).length;
+      this.promArit.push({ pos: cant + 1 + this.cantDelArit, notArit: '' });
+    }
+    else {
+      let cant = Object.keys(this.promArit).length;
+      this.promArit.push({ pos: cant + 1, notArit: '' });
+    }
+  }
+
+  guardarNotaArit() {
+    let notas: string = "";
+    for (let nota of this.promArit) {
+      if (nota.notArit != '') {
+        notas = notas + nota.notArit + "/"
+      }
+    }
+    try { this.db.guardarNotaArit(this.nombrePromArit, notas, this.userDataEmail); } catch {
+      console.log("nofunciona")
+    }
+
+  }
+
+  borrarNotaArit(numero) {
+    //Aqui busco la posición en el array del objeto a eliminar segun su variable "pos"
+    let index: number = this.promArit.indexOf(this.promArit.find(x => x.pos == numero));
+
+    this.promArit.splice(index, 1);
+    this.cantDelArit = this.cantDelArit + 1;
+    this.calcularPromArit();
+  }
+
+  agregarNotaPonde() {
+    let cant = Object.keys(this.promPonde).length;
+    this.promPonde.push({ pos: cant + 1, notPonde: 0 });
+  }
+  borrarNotaPonde(numero) {
+    //Aqui busco la posición en el array del objeto a eliminar segun su variable "pos"
+    let index: number = this.promPonde.indexOf(this.promPonde.find(x => x.pos == numero));
+
+    this.promPonde.splice(index, 1);
+  }
+
+
+
+
+  async logIn() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Cargando...', // Mensaje anim
+      spinner: 'crescent',
+    });
+
+    await loading.present();
+
+    setTimeout(() => {
+      loading.dismiss();
+      this.router.navigate(['/login']);
+
+
+      this.data = "";
+      console.log(this.data);
+    }, 1000); // Tiempo del Spinner
+  }
+  goToProfile() {
+    this.router.navigate(['/profile']);
+  }
+  tipoPromedioSelect() {
+
+    const promBas = this.promedioBasico.nativeElement;
+    const promPorc = this.promedioPorcentual.nativeElement;
+
+    if (this.tipos.tipo != "") {
+      if (this.tipos.tipo == "Aritmético") {
+        promBas.removeAttribute("hidden");
+        promPorc.setAttribute("hidden", "");
+
+
+      } else if (this.tipos.tipo == "Ponderado") {
+        promPorc.removeAttribute("hidden");
+        promBas.setAttribute("hidden", "");
+      } else if (this.tipos.tipo == "Otro") {
+        promBas.setAttribute("hidden", "");
+        promPorc.setAttribute("hidden", "");
+      }
+    }
+
+  }
 }
+
+
