65572e5984d591e35ec578fe4b8953d66b101921
danielcastillopalma <107126188+danielcastillopalma@users.noreply.github.com>
2023-11-13 12:56:24 -0300

    cambios

--- src/app/services/gameservices/sudoku.service.ts
+++ src/app/services/gameservices/sudoku.service.ts
@@ -8,7 +8,7 @@ export class SudokuService {
   constructor() { }
 
   async generateBoard(){
-    const data= await fetch("https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value}}}");
+    const data= await fetch("https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value,solution,difficulty},results,message}}");
     return data.json();
   }
 }
