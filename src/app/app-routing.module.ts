cfa714007192df5b30337f901648064a43603c96
marcuslara123 <ma.larao@duocuc.cl>
2023-11-28 23:03:28 -0400

    #

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,44 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NoIngresadoGuard } from './guards/no-ingresado.guard';
+import { IngresadoGuard } from './guards/ingresado.guard';
 
 const routes: Routes = [
   
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    canActivate: [IngresadoGuard]
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
+    canActivate: [NoIngresadoGuard]
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule),
+    canActivate: [IngresadoGuard]
   },
   {
     path: 'noteblock',
-    loadChildren: () => import('./pages/noteblock/noteblock.module').then( m => m.NoteblockPageModule)
+    loadChildren: () => import('./pages/noteblock/noteblock.module').then( m => m.NoteblockPageModule),
+    canActivate: [IngresadoGuard]
   },
   {
     path: 'notfound',
-    loadChildren: () => import('./pages/notfound/notfound.module').then( m => m.NotfoundPageModule)
+    loadChildren: () => import('./pages/notfound/notfound.module').then( m => m.NotfoundPageModule),
+    canActivate: [IngresadoGuard]
   },
   {
     path: 'tictactoe',
-    loadChildren: () => import('./pages/tictactoe/tictactoe.module').then( m => m.TictactoePageModule)
+    loadChildren: () => import('./pages/tictactoe/tictactoe.module').then( m => m.TictactoePageModule),
+    canActivate: [IngresadoGuard]
   },
 
 
