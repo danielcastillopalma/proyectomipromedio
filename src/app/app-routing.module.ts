54e4b2381c336789381dacd3bdcc0238d2cbb1f9
danielcastillopalma <d.castillo3@duocuc.cl>
2023-11-30 19:52:49 -0300

    router guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,62 +1,66 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NoIngresadoGuard } from './guards/no-ingresado.guard';
+import { IngresadoGuard } from './guards/ingresado.guard';
 
 const routes: Routes = [
-  
+
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
+    canActivate: [NoIngresadoGuard]
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule),
+    canActivate: [NoIngresadoGuard]
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfilePageModule),
+    canActivate: [IngresadoGuard]
   },
   {
     path: 'noteblock',
-    loadChildren: () => import('./pages/noteblock/noteblock.module').then( m => m.NoteblockPageModule)
+    loadChildren: () => import('./pages/noteblock/noteblock.module').then(m => m.NoteblockPageModule),
+    canActivate: [IngresadoGuard]
   },
   {
     path: 'mispromedios',
-    loadChildren: () => import('./pages/mispromedios/mispromedios.module').then( m => m.MispromediosPageModule)
+    loadChildren: () => import('./pages/mispromedios/mispromedios.module').then(m => m.MispromediosPageModule),
+    canActivate: [IngresadoGuard]
   },
+
   {
     path: 'tictactoe',
-    loadChildren: () => import('./pages/tictactoe/tictactoe.module').then( m => m.TictactoePageModule)
+    loadChildren: () => import('./pages/tictactoe/tictactoe.module').then(m => m.TictactoePageModule),
+    canActivate: [IngresadoGuard]
   },
   {
     path: 'pasatiempos',
-    loadChildren: () => import('./pages/pasatiempos/pasatiempos.module').then( m => m.PasatiemposPageModule)
+    loadChildren: () => import('./pages/pasatiempos/pasatiempos.module').then(m => m.PasatiemposPageModule),
+    canActivate: [IngresadoGuard]
   },
   {
     path: 'sudoku',
-    loadChildren: () => import('./pages/sudoku/sudoku.module').then( m => m.SudokuPageModule)
+    loadChildren: () => import('./pages/sudoku/sudoku.module').then(m => m.SudokuPageModule),
+    canActivate: [IngresadoGuard]
   },
   {
     path: '**',
-    loadChildren: () => import('./pages/notfound/notfound.module').then( m => m.NotfoundPageModule)
+    loadChildren: () => import('./pages/notfound/notfound.module').then(m => m.NotfoundPageModule),
+    canActivate: [NoIngresadoGuard]
   },
-  
-
- 
-
-  
 
 
 
- 
-  
 
-  
-  
+
 ];
 
 @NgModule({
@@ -66,6 +70,6 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 
-export class AppRoutingModule { 
-  
+export class AppRoutingModule {
+
 }
